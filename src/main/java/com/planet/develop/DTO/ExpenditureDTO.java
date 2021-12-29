6fe57eb79c7da01e07b04877aa7a0f8a0ee0c55c
zinzo1019 <topjoy22@naver.com>
2021-12-29 19:52:37 +0900

    String 타입이었던 ex_way와 ex_type을 enum 타입으로 고침

--- src/main/java/com/planet/develop/DTO/ExpenditureDTO.java
+++ src/main/java/com/planet/develop/DTO/ExpenditureDTO.java
@@ -1,6 +1,8 @@
 package com.planet.develop.DTO;
 
 import com.planet.develop.Enum.EcoEnum;
+import com.planet.develop.Enum.money_Type;
+import com.planet.develop.Enum.money_Way;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -15,8 +17,8 @@ public class ExpenditureDTO {
     private String userId; // 사용자 아이디
     private double cost; // 비용
     private LocalDateTime date; // 날짜
-    private String exType; // 유형
-    private String exWay; // 방법
+    private money_Type exType; // 유형
+    private money_Way exWay; // 방법
     private String memo; // 메모
     private EcoEnum eco; // 친/반환경
     private String ecoDetail; // 친/반환경 상세
