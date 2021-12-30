1361ec0cf0c88072a3ddf3be7791fdea5866e63d
zinzo1019 <topjoy22@naver.com>
2021-12-30 16:30:49 +0900

    회원가입 테스트 코드 작성

--- src/test/java/com/planet/develop/Service/ExpenditureTest.java
+++ src/test/java/com/planet/develop/Service/ExpenditureTest.java
@@ -1,4 +1,27 @@
 package com.planet.develop.Service;
 
+import com.planet.develop.DTO.UserDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
 public class ExpenditureTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void 회원가입() {
+        IntStream.rangeClosed(1, 10).forEach(i -> {
+            UserDTO userDTO = UserDTO.builder()
+                    .userId("user" + i + "@naver.com")
+                    .userName("user" + i)
+                    .build();
+            userService.register(userDTO);
+        });
+    }
+
 }
