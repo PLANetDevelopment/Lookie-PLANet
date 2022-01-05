d6ad666db527db778c1db4a8e1895d53fe07d532
zinzo1019 <topjoy22@naver.com>
2022-01-05 16:12:46 +0900

    친/반환경 별 총액 구하기

--- src/main/java/com/planet/develop/Service/ExpenditureDetailService.java
+++ src/main/java/com/planet/develop/Service/ExpenditureDetailService.java
@@ -2,10 +2,14 @@ package com.planet.develop.Service;
 
 import com.planet.develop.DTO.ExpenditureDTO;
 import com.planet.develop.Entity.ExpenditureDetail;
+import com.planet.develop.Entity.User;
+import com.planet.develop.Enum.EcoEnum;
 
 public interface ExpenditureDetailService {
 
     Long register(ExpenditureDTO dto);
+
+    String totalEco(User user, EcoEnum eco);
 
     default ExpenditureDetail dtoToEntity(ExpenditureDTO dto) {
         ExpenditureDetail entity = ExpenditureDetail.builder()
--- src/main/java/com/planet/develop/Service/ExpenditureDetailServiceImpl.java
+++ src/main/java/com/planet/develop/Service/ExpenditureDetailServiceImpl.java
@@ -2,23 +2,40 @@ package com.planet.develop.Service;
 
 import com.planet.develop.DTO.ExpenditureDTO;
 import com.planet.develop.Entity.ExpenditureDetail;
+import com.planet.develop.Entity.User;
+import com.planet.develop.Enum.EcoEnum;
 import com.planet.develop.Repository.ExpenditureDetailRepository;
+import com.planet.develop.Repository.ExpenditureRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Log4j2
 public class ExpenditureDetailServiceImpl implements ExpenditureDetailService {
 
-    private final ExpenditureDetailRepository repository;
+    private final ExpenditureRepository expenditureRepository;
+    private final ExpenditureDetailRepository detailRepository;
 
     @Override
     public Long register(ExpenditureDTO dto) {
         ExpenditureDetail entity = dtoToEntity(dto);
-        repository.save(entity);
+        detailRepository.save(entity);
         return entity.getEno();
+    }
+
+    @Override
+    public String totalEco(User user, EcoEnum eco) {
+        double total = 0;
+        List<Object[]> ecoList = expenditureRepository.getEcoList(user, eco);
+        for (Object[] arr : ecoList) {
+            double amount = (double) arr[1];
+            total += amount;
+        }
+        return String.format("%.0f", total);
     }
 
 }
