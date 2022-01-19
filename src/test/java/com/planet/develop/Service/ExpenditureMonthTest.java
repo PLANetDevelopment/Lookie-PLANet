633725c6d76116067b32c9e9f6f5731187bbd198
zinzo1019 <topjoy22@naver.com>
2022-01-19 13:10:33 +0900

    한 달 지출 내역 테스트 코드 작성

--- src/test/java/com/planet/develop/Service/ExpenditureMonthTest.java
+++ src/test/java/com/planet/develop/Service/ExpenditureMonthTest.java
@@ -3,6 +3,9 @@ package com.planet.develop.Service;
 import com.planet.develop.DTO.ExpenditureDTO;
 import com.planet.develop.Entity.Expenditure;
 import com.planet.develop.Entity.User;
+import com.planet.develop.Enum.EcoEnum;
+import com.planet.develop.Enum.money_Type;
+import com.planet.develop.Enum.money_Way;
 import com.planet.develop.Repository.ExpenditureDetailRepository;
 import com.planet.develop.Repository.ExpenditureRepository;
 import org.junit.jupiter.api.Test;
@@ -40,6 +43,63 @@ public class ExpenditureMonthTest {
             ExpenditureDTO expenditureDTO = expenditureService.entityToDto(e);
             System.out.println("userID: " + expenditureDTO.getUserId() + " cost: "
                     + expenditureDTO.getCost() + " date: " + expenditureDTO.getDate());
+        }
+    }
+
+    /**
+     * 사용자 아이디: user1@naver.com
+     * 지출 유형: study
+     */
+    @Test
+    public void 사용자의_한달_지출유형별_사용내역_구하기() {
+        User user = User.builder()
+                .userId("user1@naver.com")
+                .build();
+        money_Type type = money_Type.study;
+        List<Expenditure> monthList = detailService.totalMonthExType(user, 1, type);
+        System.out.println("------지출 리스트------");
+        for (Expenditure e : monthList) {
+            ExpenditureDTO expenditureDTO = expenditureService.entityToDto(e);
+            System.out.println("userID: " + expenditureDTO.getUserId() + " cost: "
+                    + expenditureDTO.getCost() + " date: " + expenditureDTO.getDate() + " exType: " + type);
+        }
+    }
+
+    /**
+     * 사용자 아이디: user1@naver.com
+     * 지출 방법: card
+     */
+    @Test
+    public void 사용자의_한달_지출방법별_사용내역_구하기() {
+        User user = User.builder()
+                .userId("user1@naver.com")
+                .build();
+        money_Way way = money_Way.card;
+        List<Expenditure> monthList = detailService.totalMonthExWay(user, 1, way);
+        System.out.println("------지출 리스트------");
+        for (Expenditure e : monthList) {
+            ExpenditureDTO expenditureDTO = expenditureService.entityToDto(e);
+            System.out.println("userID: " + expenditureDTO.getUserId() + " cost: "
+                    + expenditureDTO.getCost() + " date: " + expenditureDTO.getDate() + " exWay: " + way);
+        }
+    }
+
+    /**
+     * 사용자 아이디: user1@naver.com
+     * 친/반환경: G
+     */
+    @Test
+    public void 사용자의_한달_친반환경별_사용내역_구하기() {
+        User user = User.builder()
+                .userId("user1@naver.com")
+                .build();
+        EcoEnum eco = EcoEnum.G;
+        List<Expenditure> monthList = detailService.totalMonthEco(user, 1, eco);
+        System.out.println("------지출 리스트------");
+        for (Expenditure e : monthList) {
+            ExpenditureDTO expenditureDTO = expenditureService.entityToDto(e);
+            System.out.println("userID: " + expenditureDTO.getUserId() + " cost: "
+                    + expenditureDTO.getCost() + " date: " + expenditureDTO.getDate() + " eco: " + eco);
         }
     }
 
