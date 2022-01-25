1ef672354566e2917ecf5c376164f657750c91f4
zinzo1019 <topjoy22@naver.com>
2022-01-26 01:14:21 +0900

    ExpenditureRequestDto 클래스와 ExpenditureResponseDto 클래스 생성 (지출 데이터 요청과 응답)

---
+++ src/main/java/com/planet/develop/DTO/ExpenditureRequestDto.java
@@ -0,0 +1,24 @@
+package com.planet.develop.DTO;
+
+import com.planet.develop.Enum.EcoEnum;
+import com.planet.develop.Enum.money_Type;
+import com.planet.develop.Enum.money_Way;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExpenditureRequestDto {
+    private String userId; // 사용자 아이디
+    private double cost; // 비용
+    private LocalDate date; // 날짜
+    private money_Type exType; // 유형
+    private money_Way exWay; // 방법
+    private String memo; // 메모
+    private EcoEnum eco; // 친/반환경
+    private String ecoDetail; // 친/반환경 상세
+}
