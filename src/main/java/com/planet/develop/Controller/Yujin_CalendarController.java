6224161e2026bec5f8559da6301bf021ed04c02d
zinzo1019 <topjoy22@naver.com>
2022-01-27 20:40:07 +0900

    CalendarController의 무거운 코드를 CalendarService를 사용함으로써 가볍게 고침

--- src/main/java/com/planet/develop/Controller/Yujin_CalendarController.java
+++ src/main/java/com/planet/develop/Controller/Yujin_CalendarController.java
@@ -1,11 +1,10 @@
 package com.planet.develop.Controller;
 
-import com.planet.develop.DTO.CalendarDayDto;
 import com.planet.develop.DTO.CalendarDto;
 import com.planet.develop.DTO.IncomeDetailDto;
 import com.planet.develop.Entity.Income;
-import com.planet.develop.Entity.User;
 import com.planet.develop.Repository.UserRepository;
+import com.planet.develop.Service.CalendarService;
 import com.planet.develop.Service.ExpenditureDetailService;
 import com.planet.develop.Service.IncomeService;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,26 +24,14 @@ public class Yujin_CalendarController {
     private final IncomeService incomeService;
     private final ExpenditureDetailService detailService;
     private final UserRepository userRepository;
+    private final CalendarService calendarService;
 
     /** 월별 조회 함수 */
     @GetMapping("/api/calendar/{id}/{month}")
-    public CalendarDto findIncome(@PathVariable("id") String id, @PathVariable("month") int month){
-        Long totalMonthIncome = incomeService.totalMonth(id,month);
-        System.out.println("total: " + totalMonthIncome);
-        User user = userRepository.findById(id).get();
-        System.out.println("user: " + user);
-        Long totalMonthExpenditure = detailService.totalMonth(user, month);
-        System.out.println("total: " + totalMonthExpenditure);
-        List<CalendarDayDto> calendarDayDtos = new ArrayList<>();
-        for(int n=1;n<=31;n++) {
-            Long incomeDay = incomeService.totalDay(id, LocalDate.of(2022, month,n));
-            Long expenditureDay = detailService.totalDay(user, LocalDate.of(2022, month, n));
-            CalendarDayDto calendarDayDto = new CalendarDayDto(LocalDate.of(2022, month,n), incomeDay, expenditureDay);
-            calendarDayDtos.add(calendarDayDto);
-        }
-        return new CalendarDto(totalMonthIncome,totalMonthExpenditure, calendarDayDtos);
+    public CalendarDto findCalendar(@PathVariable("id") String id, @PathVariable("month") int month){
+        CalendarDto calendar = calendarService.findCalendar(id, month);
+        return calendar;
     }
-
 
     /** 일별 조회 (세부 조회) */
     @GetMapping("/api/calendar/{id}/{month}/{day}")
