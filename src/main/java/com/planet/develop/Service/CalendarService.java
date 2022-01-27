e7ff757421b3bd533fef3b8837641f72d902499d
zinzo1019 <topjoy22@naver.com>
2022-01-27 20:40:20 +0900

    Calender 관련 함수 작성

--- src/main/java/com/planet/develop/Service/CalendarService.java
+++ src/main/java/com/planet/develop/Service/CalendarService.java
@@ -1,4 +1,8 @@
 package com.planet.develop.Service;
 
-public class CalendarService {
+import com.planet.develop.DTO.CalendarDto;
+
+public interface CalendarService {
+    /** 월별 지출/수입 총액과 일별 지출/소비 총액 구하기 */
+    CalendarDto findCalendar(String id, int month);
 }
--- src/main/java/com/planet/develop/Service/CalendarServiceImpl.java
+++ src/main/java/com/planet/develop/Service/CalendarServiceImpl.java
@@ -1,4 +1,39 @@
 package com.planet.develop.Service;
 
-public class CalendarServiceImpl {
+import com.planet.develop.DTO.CalendarDayDto;
+import com.planet.develop.DTO.CalendarDto;
+import com.planet.develop.Entity.User;
+import com.planet.develop.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class CalendarServiceImpl implements CalendarService {
+
+    private final IncomeService incomeService;
+    private final ExpenditureDetailService detailService;
+    private final UserRepository userRepository;
+
+    @Override
+    public CalendarDto findCalendar(String id, int month) {
+        Long totalMonthIncome = incomeService.totalMonth(id,month);
+        User user = userRepository.findById(id).get();
+        Long totalMonthExpenditure = detailService.totalMonth(user, month);
+        List<CalendarDayDto> calendarDayDtos = new ArrayList<>();
+        for(int n=1;n<=31;n++) {
+            Long incomeDay = incomeService.totalDay(id, LocalDate.of(2022, month,n));
+            Long expenditureDay = detailService.totalDay(user, LocalDate.of(2022, month, n));
+            CalendarDayDto calendarDayDto = new CalendarDayDto(LocalDate.of(2022, month,n), incomeDay, expenditureDay);
+            calendarDayDtos.add(calendarDayDto);
+        }
+        return new CalendarDto(totalMonthIncome,totalMonthExpenditure, calendarDayDtos);
+    }
+
 }
