e5d4a6c658e945fb0e729232b996f940d847b8cc
zinzo1019 <topjoy22@naver.com>
2022-01-31 12:06:08 +0900

    ExpenditureDetailDto 클래스 생성

---
+++ src/main/java/com/planet/develop/DTO/ExpenditureDetailDto.java
@@ -0,0 +1,20 @@
+package com.planet.develop.DTO;
+
+import com.planet.develop.Enum.EcoEnum;
+import com.planet.develop.Enum.money_Type;
+import com.planet.develop.Enum.money_Way;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExpenditureDetailDto {
+    private Long cost; // 비용
+    private money_Type exType; // 유형
+    private money_Way exWay; // 방법
+    private String memo; // 메모
+    private EcoEnum eco; // 친/반환경
+    private String ecoDetail; // 친/반환경 상세
+}
