854a45e817ebb7bcd00f3913526096ed8ca9bd5c
chaewon <cococode12@gmail.com>
2022-02-10 22:24:17 +0900

    type별로 일별 조회 분류

--- src/main/java/com/planet/develop/Controller/CalendarController.java
+++ src/main/java/com/planet/develop/Controller/CalendarController.java
@@ -4,10 +4,7 @@ import com.planet.develop.DTO.IncomeDetailDto;
 import com.planet.develop.DTO.IncomeDto;
 import com.planet.develop.Entity.Income;
 import com.planet.develop.Enum.money_Type;
-import com.planet.develop.Enum.money_Way;
 import com.planet.develop.Service.IncomeService;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 
@@ -43,13 +42,36 @@ public class CalendarController {
 
     /** 일별 조회 (세부 조회) */
     @GetMapping("/api/income/{id}/{month}/{day}")
-    public List<IncomeDetailDto> findIncomeDetail(@PathVariable("id") String id,@PathVariable("month") int month,@PathVariable("day") int day){
+    public Map<String,IncomeDetailDto> findIncomeDetail(@PathVariable("id") String id,@PathVariable("month") int month,@PathVariable("day") int day){
         List<Income> days = incomeService.findDay(id, LocalDate.of(2022, month, day));
 
         List<IncomeDetailDto> collect = days.stream()
                 .map(m->new IncomeDetailDto(m.getIn_cost(),m.getIn_way(),m.getIn_type(),m.getMemo()))
                 .collect(Collectors.toList());
 
-        return collect;
+        Map incomeDetails = sortByType(collect);
+        return incomeDetails;
+    }
+
+    private Map sortByType(List<IncomeDetailDto> collect) {
+        List<IncomeDetailDto> salary=new ArrayList<>();
+        List<IncomeDetailDto> allowance=new ArrayList<>();
+        List<IncomeDetailDto> etc=new ArrayList<>();
+
+        for (IncomeDetailDto detailDto : collect) {
+            if (detailDto.getIn_type().equals(money_Type.salary))
+                salary.add(detailDto);
+            else if (detailDto.getIn_type().equals(money_Type.allowance))
+                allowance.add(detailDto);
+            else if (detailDto.getIn_type().equals(money_Type.etc))
+                etc.add(detailDto);
+        }
+
+        Map types = new HashMap();
+        types.put("salary", salary);
+        types.put("allowance", allowance);
+        types.put("etc", etc);
+
+        return types;
     }
 }
