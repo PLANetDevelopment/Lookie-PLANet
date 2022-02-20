2b39fb2e646048321c94f0fe27ddb7ddfef14c3f
zinzo1019 <topjoy22@naver.com>
2022-02-20 17:26:08 +0900

    eco entity 생성

--- src/main/java/com/planet/develop/Entity/Eco.java
+++ src/main/java/com/planet/develop/Entity/Eco.java
@@ -1,2 +1,40 @@
-package com.planet.develop.Entity;public class Eco {
+package com.planet.develop.Entity;
+
+import com.planet.develop.Enum.EcoDetail;
+import com.planet.develop.Enum.EcoEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Eco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long eno;
+
+    @Enumerated(EnumType.STRING)
+    private EcoEnum eco;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "eco_detail")
+    private EcoDetail ecoDetail;
+
+    private String etcMemo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Expenditure expenditure;
+
+    public void update(EcoDetail ecoDetail, String etcMemo) {
+        this.ecoDetail = this.getEcoDetail();
+        this.etcMemo = this.getEtcMemo();
+    }
+
 }
