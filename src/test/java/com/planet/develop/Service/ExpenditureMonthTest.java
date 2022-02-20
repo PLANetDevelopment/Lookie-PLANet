19c8535301680fe11cac9d7ab0030ef03b82cb6c
zinzo1019 <topjoy22@naver.com>
2022-02-20 17:26:30 +0900

    eco 테이블 테스트 코드 작성

--- src/test/java/com/planet/develop/Service/ExpenditureMonthTest.java
+++ src/test/java/com/planet/develop/Service/ExpenditureMonthTest.java
@@ -24,87 +24,87 @@ public class ExpenditureMonthTest {
 
     int month = 1;
 
-    /**
-     * 사용자 아이디: user1@naver.com
-     * 한 달 사용 내역
-     */
-    @Test
-    public void 사용자의_한달_사용내역_구하기() {
-        User user = User.builder()
-                .userId("user1@naver.com")
-                .build();
-        List<Expenditure> monthList = detailService.getMonthList(user, 1);
-        System.out.println("------지출 리스트------");
-        for (Expenditure e : monthList) {
-            ExpenditureRequestDto ExpenditureRequestDto = expenditureService.entityToDto(e);
-            System.out.println("userID: " + ExpenditureRequestDto.getUserId() + " cost: "
-                    + ExpenditureRequestDto.getCost() + " date: " + ExpenditureRequestDto.getDate());
-        }
-        System.out.println(user.getUserId() + " 님의 " + month + "월 "
-                + "총 지출 금액은 " + detailService.totalMonth(user, month) + "원 입니다.");
-    }
-
-    /**
-     * 사용자 아이디: user1@naver.com
-     * 지출 유형: study
-     */
-    @Test
-    public void 사용자의_한달_지출유형별_사용내역_구하기() {
-        User user = User.builder()
-                .userId("user1@naver.com")
-                .build();
-        money_Type type = money_Type.study;
-        List<Expenditure> monthList = detailService.getMonthTypeList(user, month, type);
-        System.out.println("------지출 리스트------");
-        for (Expenditure e : monthList) {
-            ExpenditureRequestDto ExpenditureRequestDto = expenditureService.entityToDto(e);
-            System.out.println("userID: " + ExpenditureRequestDto.getUserId() + " cost: "
-                    + ExpenditureRequestDto.getCost() + " date: " + ExpenditureRequestDto.getDate() + " exType: " + type);
-        }
-        System.out.println(user.getUserId() + " 님의 " + month + "월 " + type + "별"
-                + " 총 지출 금액은 " + detailService.totalMonthType(user, month, type) + "원 입니다.");
-    }
-
-    /**
-     * 사용자 아이디: user1@naver.com
-     * 지출 방법: card
-     */
-    @Test
-    public void 사용자의_한달_지출방법별_사용내역_구하기() {
-        User user = User.builder()
-                .userId("user1@naver.com")
-                .build();
-        money_Way way = money_Way.card;
-        List<Expenditure> monthList = detailService.getMonthWayList(user, month, way);
-        System.out.println("------지출 리스트------");
-        for (Expenditure e : monthList) {
-            ExpenditureRequestDto ExpenditureRequestDto = expenditureService.entityToDto(e);
-            System.out.println("userID: " + ExpenditureRequestDto.getUserId() + " cost: "
-                    + ExpenditureRequestDto.getCost() + " date: " + ExpenditureRequestDto.getDate() + " exWay: " + way);
-        }
-        System.out.println(user.getUserId() + " 님의 " + month + "월 " + way + "별"
-                + " 총 지출 금액은 " + detailService.totalWayMonth(user, month, way) + "원 입니다.");
-    }
-
-    /**
-     * 사용자 아이디: user1@naver.com
-     * 친/반환경: G
-     */
-    @Test
-    public void 사용자의_한달_친반환경별_사용내역_구하기() {
-        User user = User.builder()
-                .userId("user1@naver.com")
-                .build();
-        EcoEnum eco = EcoEnum.G;
-        List<Expenditure> monthList = detailService.getMonthEcoList(user, month, eco);
-        System.out.println("------지출 리스트------");
-        for (Expenditure e : monthList) {
-            ExpenditureRequestDto ExpenditureRequestDto = expenditureService.entityToDto(e);
-            System.out.println("userID: " + ExpenditureRequestDto.getUserId() + " cost: "
-                    + ExpenditureRequestDto.getCost() + " date: " + ExpenditureRequestDto.getDate() + " eco: " + eco);
-        }
-        System.out.println(user.getUserId() + " 님의 " + month + "월 " + eco + "별"
-                + " 총 지출 금액은 " + detailService.totalEcoMonth(user, month, eco) + "원 입니다.");
-    }
+//    /**
+//     * 사용자 아이디: user1@naver.com
+//     * 한 달 사용 내역
+//     */
+//    @Test
+//    public void 사용자의_한달_사용내역_구하기() {
+//        User user = User.builder()
+//                .userId("user1@naver.com")
+//                .build();
+//        List<Expenditure> monthList = detailService.getMonthList(user, 1);
+//        System.out.println("------지출 리스트------");
+//        for (Expenditure e : monthList) {
+//            ExpenditureRequestDto ExpenditureRequestDto = expenditureService.entityToDto(e);
+//            System.out.println("userID: " + ExpenditureRequestDto.getUserId() + " cost: "
+//                    + ExpenditureRequestDto.getCost() + " date: " + ExpenditureRequestDto.getDate());
+//        }
+//        System.out.println(user.getUserId() + " 님의 " + month + "월 "
+//                + "총 지출 금액은 " + detailService.totalMonth(user, month) + "원 입니다.");
+//    }
+//
+//    /**
+//     * 사용자 아이디: user1@naver.com
+//     * 지출 유형: study
+//     */
+//    @Test
+//    public void 사용자의_한달_지출유형별_사용내역_구하기() {
+//        User user = User.builder()
+//                .userId("user1@naver.com")
+//                .build();
+//        money_Type type = money_Type.study;
+//        List<Expenditure> monthList = detailService.getMonthTypeList(user, month, type);
+//        System.out.println("------지출 리스트------");
+//        for (Expenditure e : monthList) {
+//            ExpenditureRequestDto ExpenditureRequestDto = expenditureService.entityToDto(e);
+//            System.out.println("userID: " + ExpenditureRequestDto.getUserId() + " cost: "
+//                    + ExpenditureRequestDto.getCost() + " date: " + ExpenditureRequestDto.getDate() + " exType: " + type);
+//        }
+//        System.out.println(user.getUserId() + " 님의 " + month + "월 " + type + "별"
+//                + " 총 지출 금액은 " + detailService.totalMonthType(user, month, type) + "원 입니다.");
+//    }
+//
+//    /**
+//     * 사용자 아이디: user1@naver.com
+//     * 지출 방법: card
+//     */
+//    @Test
+//    public void 사용자의_한달_지출방법별_사용내역_구하기() {
+//        User user = User.builder()
+//                .userId("user1@naver.com")
+//                .build();
+//        money_Way way = money_Way.card;
+//        List<Expenditure> monthList = detailService.getMonthWayList(user, month, way);
+//        System.out.println("------지출 리스트------");
+//        for (Expenditure e : monthList) {
+//            ExpenditureRequestDto ExpenditureRequestDto = expenditureService.entityToDto(e);
+//            System.out.println("userID: " + ExpenditureRequestDto.getUserId() + " cost: "
+//                    + ExpenditureRequestDto.getCost() + " date: " + ExpenditureRequestDto.getDate() + " exWay: " + way);
+//        }
+//        System.out.println(user.getUserId() + " 님의 " + month + "월 " + way + "별"
+//                + " 총 지출 금액은 " + detailService.totalWayMonth(user, month, way) + "원 입니다.");
+//    }
+//
+//    /**
+//     * 사용자 아이디: user1@naver.com
+//     * 친/반환경: G
+//     */
+//    @Test
+//    public void 사용자의_한달_친반환경별_사용내역_구하기() {
+//        User user = User.builder()
+//                .userId("user1@naver.com")
+//                .build();
+//        EcoEnum eco = EcoEnum.G;
+//        List<Expenditure> monthList = detailService.getMonthEcoList(user, month, eco);
+//        System.out.println("------지출 리스트------");
+//        for (Expenditure e : monthList) {
+//            ExpenditureRequestDto ExpenditureRequestDto = expenditureService.entityToDto(e);
+//            System.out.println("userID: " + ExpenditureRequestDto.getUserId() + " cost: "
+//                    + ExpenditureRequestDto.getCost() + " date: " + ExpenditureRequestDto.getDate() + " eco: " + eco);
+//        }
+//        System.out.println(user.getUserId() + " 님의 " + month + "월 " + eco + "별"
+//                + " 총 지출 금액은 " + detailService.totalEcoMonth(user, month, eco) + "원 입니다.");
+//    }
     
 }
--- src/test/java/com/planet/develop/Service/ExpenditureTest.java
+++ src/test/java/com/planet/develop/Service/ExpenditureTest.java
@@ -2,9 +2,9 @@ package com.planet.develop.Service;
 
 import com.planet.develop.DTO.ExpenditureRequestDto;
 import com.planet.develop.DTO.UserDTO;
+import com.planet.develop.Entity.Expenditure;
 import com.planet.develop.Entity.ExpenditureDetail;
-import com.planet.develop.Entity.User;
-import com.planet.develop.Enum.EcoEnum;
+import com.planet.develop.Enum.EcoDetail;
 import com.planet.develop.Enum.money_Type;
 import com.planet.develop.Enum.money_Way;
 import com.planet.develop.Repository.ExpenditureDetailRepository;
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
@@ -32,12 +32,14 @@ public class ExpenditureTest {
     private ExpenditureRepository expenditureRepository;
     @Autowired
     private ExpenditureDetailRepository detailRepository;
+    @Autowired
+    private EcoService ecoService;
 
     Random random = new Random();
 
     @Test
     public void 회원가입() {
-        IntStream.rangeClosed(1, 10).forEach(i -> {
+        IntStream.rangeClosed(1, 5).forEach(i -> {
             UserDTO userDTO = UserDTO.builder()
                     .userId("user" + i + "@naver.com")
                     .userName("user" + i)
@@ -53,80 +55,87 @@ public class ExpenditureTest {
      */
     @Test
     public void 지출_데이터_삽입() {
-        IntStream.rangeClosed(1, 10).forEach(i -> {
+        // ecoDetail 리스트 설정
+        List<EcoDetail> ecoDetails = new ArrayList<>();
+        ecoDetails.add(EcoDetail.multiUse);
+        ecoDetails.add(EcoDetail.personalBag);
+        ecoDetails.add(EcoDetail.wasteFood);
+
+        IntStream.rangeClosed(1, 5).forEach(i -> {
             ExpenditureRequestDto dto = ExpenditureRequestDto.builder()
-                    .cost(Long.valueOf(random.nextInt(100000))) // 가격 랜덤 삽입
-                    .eco(EcoEnum.values()[new Random().nextInt(EcoEnum.values().length)]) // 친/반환경 랜덤 삽입
-                    .ecoDetail("good detail")
+                    .ex_cost(Long.valueOf(random.nextInt(100000))) // 가격 랜덤 삽입
+                    .ecoDetail(ecoDetails)
+                    .etcMemo("etc memo")
                     .exType(money_Type.values()[new Random().nextInt(money_Type.values().length)]) // 유형 랜덤 삽입
                     .exWay(money_Way.values()[new Random().nextInt(money_Way.values().length)]) // 방법 랜덤 삽입
                     .memo("good memo")
                     .date(LocalDate.of(2022, 01, 23))
-                    .userId("user" + (int)(Math.random()*10+1) + "@naver.com") // 사용자 아이디 랜덤 삽입
+                    .userId("user" + (int)(Math.random()*3+1) + "@naver.com") // 사용자 아이디 랜덤 삽입
                     .build();
+
             Long deno = detailService.save(dto);
-            System.out.println("expenditrue detail table: " + deno);
             ExpenditureDetail detail = detailRepository.findById(deno).get();
             Long eno = expenditureService.save(dto, detail);
-            System.out.println("expenditure table: " + eno);
+            Expenditure expenditure = expenditureRepository.findById(eno).get();
+            ecoService.save(dto, expenditure);
         });
     }
-
-    /**
-     * 사용자 아이디: user10@naver.com
-     * 친반환경: G
-     */
-    @Test
-    public void 사용자별_하루_친반환경_지출내역_가져오기() {
-        User user = User.builder()
-                .userId("user10@naver.com")
-                .build();
-        EcoEnum eco = EcoEnum.G;
-        LocalDate date = LocalDate.now();
-        List<Object[]> ecoList = expenditureRepository.getDayEcoList(user, eco, date);
-        System.out.println("-----지출 리스트-----");
-        for (Object[] arr : ecoList)
-            System.out.println(Arrays.toString(arr));
-        System.out.println(user.getUserId() + " 님의 " + date + " " + eco + " 별"
-                + " 총 지출 금액은 " + detailService.totalEcoDay(user, eco, date) + "원 입니다.");
-    }
-
-    /**
-     * 사용자 아이디: user1@naver.com
-     * 지출 유형: study
-     */
-    @Test
-    public void 사용자별_하루_카테고리별_지출내역_가져오기() {
-        User user = User.builder()
-                .userId("user1@naver.com")
-                .build();
-        money_Type type = money_Type.study;
-        LocalDate date = LocalDate.now();
-        List<Object[]> ecoList = expenditureRepository.getDayExTypeList(user, type, date);
-        System.out.println("-----지출 리스트-----");
-        for (Object[] arr : ecoList)
-            System.out.println(Arrays.toString(arr));
-        System.out.println(user.getUserId() + " 님의 " + date + " " + type + "별"
-                + " 총 지출 금액은 " + detailService.totalTypeDay(user, type, date) + "원 입니다.");
-    }
-
-    /**
-     * 사용자 아이디: user1@naver.com
-     * 지출 방법: card
-     */
-    @Test
-    public void 사용자별_하루_지출방법별_지출내역_가져오기() {
-        User user = User.builder()
-                .userId("user1@naver.com")
-                .build();
-        money_Way way = money_Way.card;
-        LocalDate date = LocalDate.now();
-        List<Object[]> ecoList = expenditureRepository.getDayExWayList(user, way, date);
-        System.out.println("---------지출 리스트----------");
-        for (Object[] arr : ecoList)
-            System.out.println(Arrays.toString(arr));
-        System.out.println(user.getUserId() + " 님의 " + date + " " + way + "별"
-                + " 총 지출 금액은 " + detailService.totalWayDay(user, way, date) + "원 입니다.");
-    }
+//
+//    /**
+//     * 사용자 아이디: user10@naver.com
+//     * 친반환경: G
+//     */
+//    @Test
+//    public void 사용자별_하루_친반환경_지출내역_가져오기() {
+//        User user = User.builder()
+//                .userId("user10@naver.com")
+//                .build();
+//        EcoEnum eco = EcoEnum.G;
+//        LocalDate date = LocalDate.now();
+//        List<Object[]> ecoList = expenditureRepository.getDayEcoList(user, eco, date);
+//        System.out.println("-----지출 리스트-----");
+//        for (Object[] arr : ecoList)
+//            System.out.println(Arrays.toString(arr));
+//        System.out.println(user.getUserId() + " 님의 " + date + " " + eco + " 별"
+//                + " 총 지출 금액은 " + detailService.totalEcoDay(user, eco, date) + "원 입니다.");
+//    }
+//
+//    /**
+//     * 사용자 아이디: user1@naver.com
+//     * 지출 유형: study
+//     */
+//    @Test
+//    public void 사용자별_하루_카테고리별_지출내역_가져오기() {
+//        User user = User.builder()
+//                .userId("user1@naver.com")
+//                .build();
+//        money_Type type = money_Type.study;
+//        LocalDate date = LocalDate.now();
+//        List<Object[]> ecoList = expenditureRepository.getDayExTypeList(user, type, date);
+//        System.out.println("-----지출 리스트-----");
+//        for (Object[] arr : ecoList)
+//            System.out.println(Arrays.toString(arr));
+//        System.out.println(user.getUserId() + " 님의 " + date + " " + type + "별"
+//                + " 총 지출 금액은 " + detailService.totalTypeDay(user, type, date) + "원 입니다.");
+//    }
+//
+//    /**
+//     * 사용자 아이디: user1@naver.com
+//     * 지출 방법: card
+//     */
+//    @Test
+//    public void 사용자별_하루_지출방법별_지출내역_가져오기() {
+//        User user = User.builder()
+//                .userId("user1@naver.com")
+//                .build();
+//        money_Way way = money_Way.card;
+//        LocalDate date = LocalDate.now();
+//        List<Object[]> ecoList = expenditureRepository.getDayExWayList(user, way, date);
+//        System.out.println("---------지출 리스트----------");
+//        for (Object[] arr : ecoList)
+//            System.out.println(Arrays.toString(arr));
+//        System.out.println(user.getUserId() + " 님의 " + date + " " + way + "별"
+//                + " 총 지출 금액은 " + detailService.totalWayDay(user, way, date) + "원 입니다.");
+//    }
 
 }
