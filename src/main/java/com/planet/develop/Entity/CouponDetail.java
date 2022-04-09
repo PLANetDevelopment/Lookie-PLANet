4e6c652b42b919cde99a11f88d2dce7c14f440f7
zinzo1019 <topjoy22@naver.com>
2022-04-09 14:48:09 +0900

    [테이블 생성] 쿠폰 테이블 / 쿠폰 상세 테이블 생성

--- src/main/java/com/planet/develop/Entity/CouponDetail.java
+++ src/main/java/com/planet/develop/Entity/CouponDetail.java
@@ -1,2 +1,16 @@
-package com.planet.develop.Entity;public class CouponDetail {
+package com.planet.develop.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CouponDetail {
 }
